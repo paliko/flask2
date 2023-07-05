245c3d39ab1fd354db639173a53042de8f25f93a
paliko <57220541+paliko@users.noreply.github.com>
2023-07-05 20:49:45 +0200

    Lekce3/wt forms (#2)
    * zkouska pridanej komentar to flask 2
    
    * lekce 3 - kalkulacka s knihovnou WTForms
    
    * pridanej komentar

--- itnetwork/kalkulacka/main.py
+++ itnetwork/kalkulacka/main.py
@@ -1,42 +1,29 @@
 from flask import Flask, render_template, request
 #comment2
 
+from flask_wtf import FlaskForm
+from wtforms import SelectField, IntegerField, widgets
+
 app = Flask(__name__)
+# Musíme nastavit SECRET_KEY, pokud chceme používat CSRF
+app.config["SECRET_KEY"] = "super tajny klic"
 
-def secti(a, b):
-    return float(a) + float(b)
+class MujFormular(FlaskForm):
+    prvni_cislo = IntegerField("První Číslo", widget = widgets.Input(input_type = "number"))
+    operator = SelectField("Operátor", choices=[("+" ,"+"), ("-", "-"), ("*", "*"), ("/", "/")])
+    druhe_cislo = IntegerField("Druhé Číslo", widget = widgets.Input(input_type = "number"))
 
-def odecti(a, b):
-    return float(a) - float(b)
-
-def podil(a, b):
-    return float(a) / float(b)
-
-def soucin(a, b):
-    return float(a) * float(b)
-
-@app.route("/", methods = ["GET", "POST"]) # Povolíme metody GET a POST
+@app.route("/", methods = ["GET", "POST"])
 def kalkulacka():
-    prvni_cislo = request.form.get("prvni_cislo") #Získáme hodnotu z POST requestu
-    druhe_cislo = request.form.get("druhe_cislo")
-    operator = request.form.get("operator")
-    if prvni_cislo is None or druhe_cislo is None:
-        return render_template("template.html", vysledek = "Vyplň formulář")
-    if (float(druhe_cislo) == 0 and operator == "/"):
-        vysledek = "Nelze dělit nulou"
-        return render_template("template.html", vysledek = vysledek)
-    if (operator == "+"):
-        vysledek = secti(prvni_cislo, druhe_cislo)
-    elif (operator == "-"):
-        vysledek = odecti(prvni_cislo, druhe_cislo)
-    elif (operator == "/"):
-        vysledek = podil(prvni_cislo, druhe_cislo)
-    elif (operator == "*"):
-        vysledek = soucin(prvni_cislo, druhe_cislo)
-    else:
-        vysledek = "Chyba"
+    form=MujFormular()
+    if form.validate_on_submit(): #will check if it is a POST request and if it is valid.
+        prvni_cislo = form.prvni_cislo.data
+        druhe_cislo = form.druhe_cislo.data
+        operator = form.operator.data
+        vysledek = eval( str(prvni_cislo) + operator + str(druhe_cislo) )
+        return render_template("kalkulacka.html", vysledek = vysledek, form = form)
+    return render_template("kalkulacka.html", form = form) #Vrátíme naší šablonu s výsledkem
 
-    return render_template("kalkulacka.html", vysledek = vysledek) #Vrátíme naší šablonu s výsledkem
 
 if __name__ == "__main__":
     app.run(debug=True)
